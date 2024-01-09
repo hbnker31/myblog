72d2d45a8c7d8db66fcdf714f18532b2e47bcdc5
Hemant Bhankhar <hbnker31@gmail.com>
2024-01-09 17:59:35 +0530

    set update

--- components/markdown/CopyButton.tsx
+++ components/markdown/CopyButton.tsx
@@ -1,38 +1,46 @@
-"use client";
-import { cn } from "@/lib/utils";
-import React, { useState } from "react";
-import { BsCopy } from "react-icons/bs";
-import { IoCheckmarkOutline } from "react-icons/io5";
+'use client';
+import { cn } from '@/lib/utils';
+import React, { useState } from 'react';
+import { BsCopy } from 'react-icons/bs';
+import { IoCheckmarkOutline } from 'react-icons/io5';
 
 export default function CopyButton({ id }: { id: string }) {
-    const [onCopy, setOnCopy] = useState(false);
-    const [onDone, setOnDone] = useState(false);
-    const handleCopy = async () => {
-        console.log(id);
+  const [onCopy, setOnCopy] = useState(false);
+  const [onDone, setOnDone] = useState(false);
+  const handleCopy = async () => {
+    console.log(id);
 
-        const text = document.getElementById(id)?.textContent;
-        console.log(text);
+    const text = document.getElementById(id)?.textContent;
+    console.log(text);
 
-        try {
-            await navigator.clipboard.writeText(text!);
-            setOnCopy(true);
-        } catch {
-            console.log("copy error");
-        }
-    };
-    return (
-        <div onClick={handleCopy} className="p-2 hover:scale-105 cursor-pointer hover:bg-slate-700 rounded-md relative">
-            <IoCheckmarkOutline className={cn("cursor-pointer w-5 h-5 text-green-500 transition-all", onDone ? "scale-100" : "scale-0")} />
-            <div className="h-full w-full absolute top-0 left-0 flex items-center justify-center">
-                <BsCopy
-                    className={cn("transition-all", onCopy ? "scale-0" : "scale-100")}
-                    onTransitionEnd={() => {
-                        if (onCopy) {
-                            setOnDone(true);
-                        }
-                    }}
-                />
-            </div>
-        </div>
-    );
+    try {
+      await navigator.clipboard.writeText(text!);
+      setOnCopy(true);
+    } catch {
+      console.log('copy error');
+    }
+  };
+  return (
+    <div
+      onClick={handleCopy}
+      className="p-2 hover:scale-105 cursor-pointer hover:bg-slate-700 rounded-md relative"
+    >
+      <IoCheckmarkOutline
+        className={cn(
+          'cursor-pointer w-5 h-5 text-purple-500 transition-all',
+          onDone ? 'scale-100' : 'scale-0',
+        )}
+      />
+      <div className="h-full w-full absolute top-0 left-0 flex items-center justify-center">
+        <BsCopy
+          className={cn('transition-all', onCopy ? 'scale-0' : 'scale-100')}
+          onTransitionEnd={() => {
+            if (onCopy) {
+              setOnDone(true);
+            }
+          }}
+        />
+      </div>
+    </div>
+  );
 }
--- components/stripe/Checkout.tsx
+++ components/stripe/Checkout.tsx
@@ -1,48 +1,66 @@
-import React, { useEffect, useTransition } from "react";
-import { Button } from "../ui/button";
-import { LightningBoltIcon } from "@radix-ui/react-icons";
-import { useUser } from "@/lib/store/user";
-import LoginForm from "../nav/LoginForm";
-import { usePathname } from "next/navigation";
-import { checkout } from "@/lib/actions/stripe";
-import { cn } from "@/lib/utils";
-import { loadStripe } from "@stripe/stripe-js";
+import React, { useEffect, useTransition } from 'react';
+import { Button } from '../ui/button';
+import { LightningBoltIcon } from '@radix-ui/react-icons';
+import { useUser } from '@/lib/store/user';
+import LoginForm from '../nav/LoginForm';
+import { usePathname } from 'next/navigation';
+import { checkout } from '@/lib/actions/stripe';
+import { cn } from '@/lib/utils';
+import { loadStripe } from '@stripe/stripe-js';
 
 export default function Checkout() {
-    const pathname = usePathname();
-    const [isPending, setIsPending] = useTransition();
-    const user = useUser((state) => state.user);
-    useEffect(() => {
-        console.log("sadfjhwejf");
-        const data = fetch("https://animechan.xyz/api/random/anime?title=naruto")
-            .then((response) => response.json())
-            .then((quote) => console.log(quote));
-        console.log(data);
-    }, []);
-    const handleCheckout = (e: any) => {
-        e.preventDefault();
-        setIsPending(async () => {
-            const data = JSON.parse(await checkout(user?.email!, location.origin + pathname));
-            const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!);
-            await stripe?.redirectToCheckout({ sessionId: data.id });
-        });
-    };
+  const pathname = usePathname();
+  const [isPending, setIsPending] = useTransition();
+  const user = useUser((state) => state.user);
+  useEffect(() => {
+    console.log('sadfjhwejf');
+    const data = fetch('https://animechan.xyz/api/random/anime?title=naruto')
+      .then((response) => response.json())
+      .then((quote) => console.log(quote));
+    console.log(data);
+  }, []);
+  const handleCheckout = (e: any) => {
+    e.preventDefault();
+    setIsPending(async () => {
+      const data = JSON.parse(
+        await checkout(user?.email!, location.origin + pathname),
+      );
+      const stripe = await loadStripe(
+        process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!,
+      );
+      await stripe?.redirectToCheckout({ sessionId: data.id });
+    });
+  };
 
-    if (!user?.id) {
-        return (
-            <div className="flex items-center h-96 w-full justify-center gap-2">
-                <LoginForm /> to read
-            </div>
-        );
-    }
+  if (!user?.id) {
     return (
-        <form className={cn("h96 w-full flex items-center justify-normal", { "animate-pulse": isPending })} onSubmit={handleCheckout}>
-            <Button variant="ghost" className="flex flex-col p-10 gap-5 ring ring-green-500">
-                <span className="flex items-center gap-2 text-2xl font-blod text-green-500">
-                    <LightningBoltIcon className={cn("h-5 w-5", !isPending ? "animate-bounce" : "animate-spin")} /> Upgrade to Premium
-                </span>
-                <span>Unlock all blog contents</span>
-            </Button>
-        </form>
+      <div className="flex items-center h-96 w-full justify-center gap-2">
+        <LoginForm /> to read
+      </div>
     );
+  }
+  return (
+    <form
+      className={cn('h96 w-full flex items-center justify-normal', {
+        'animate-pulse': isPending,
+      })}
+      onSubmit={handleCheckout}
+    >
+      <Button
+        variant="ghost"
+        className="flex flex-col p-10 gap-5 ring ring-purple-500"
+      >
+        <span className="flex items-center gap-2 text-2xl font-blod text-purple-500">
+          <LightningBoltIcon
+            className={cn(
+              'h-5 w-5',
+              !isPending ? 'animate-bounce' : 'animate-spin',
+            )}
+          />{' '}
+          Upgrade to Premium
+        </span>
+        <span>Unlock all blog contents</span>
+      </Button>
+    </form>
+  );
 }
